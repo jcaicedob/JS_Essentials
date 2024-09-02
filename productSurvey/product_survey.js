66d2371479ef58ceaeb59c7ef63b76ae73754bd1
Jairo C <jcaice@hotmail.com>
2024-09-02 17:42:56 -0400

    Not workig. Review

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,6 +8,21 @@ function submitFeedback() {
     const feedback = document.getElementById('feedbackText').value;
     const submitButton = document.getElementById('submitBtn');
     alert('Thank you for your valuable feedback')
+
+    document.getElementById('userName').innerHTML = username;
+    document.getElementById('userAge').innerHTML = age;
+    document.getElementById('userEmail').innerHTML = email;
+    document.getElementById('userJob').innerHTML = job;
+    document.getElementById('userDesignation').innerHTML = designation;
+    document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userFeedback').innerHTML = feedback;
+
+    document.getElementById('userInfo').style.display = 'block';
 }
 
-submitButton.onclick = submitFeedback;
+submitButton.onclick = submitFeedback();
+document.addEventListener('keydown', function(event) {
+   if (event.key === 'Enter') {
+     submitFeedback();
+   }
+ });
