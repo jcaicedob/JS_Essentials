6749e8a5c4d7685ec6ae43621dae7d1704035804
Jairo Caicedo <jcaice@hotmail.com>
2024-09-02 10:01:08 -0500

    Fixed bug in function

--- groceryTracker/groceryTracker.js
+++ groceryTracker/groceryTracker.js
@@ -2,8 +2,8 @@ let amount1, amount2, amount3;
 
 function calculateTotal() {
     amount1 = parseFloat(document.getElementById('amount1').value);
-    amount1 = parseFloat(document.getElementById('amount2').value);
-    amount1 = parseFloat(document.getElementById('amount3').value);
+    amount2 = parseFloat(document.getElementById('amount2').value);
+    amount3 = parseFloat(document.getElementById('amount3').value);
    
     // Calculation
     let total = amount1+amount2+amount3;
