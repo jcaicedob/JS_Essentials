8f73230b8752fc445e755e5037416f0fdad8650e
Jairo C <jcaice@hotmail.com>
2024-09-09 15:02:05 -0400

    added delete function

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,8 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -37,4 +38,9 @@ function clearInputs() {
     document.getElementById('pagesNumber').value = '';
  }
 
+function deleteBook(index) {
+    console.log("Deleting book #", index)
+    books.splice(index,1)
+    showbooks()
+}
  
